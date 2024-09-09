d0135bd178c8c68cfe2bfce8a955c7172bfbd062
Harsh Bangari <harshrock2010@gmail.com>
2024-09-09 18:08:39 +0530

    code updated

--- server.js
+++ server.js
@@ -1,126 +1,42 @@
-const path = require("path");
-const express = require("express");
-const app = express();
-const socketIO = require("socket.io");
+import { WebSocketServer } from "ws";
 
-const port = process.env.PORT || 8443;
-const env = process.env.NODE_ENV || "development";
+const server = new WebSocketServer({ port: 8080 }, () =>
+  console.log("Server started at 8080")
+);
 
-var fs = require("fs");
-var https = require("https");
-var privateKey = fs.readFileSync("sslcert/key.pem", "utf8");
-var certificate = fs.readFileSync("sslcert/cert.pem", "utf8");
+const users = new Set();
 
-var credentials = { key: privateKey, cert: certificate };
+function sendMessage(message) {
+  users.forEach((user) => {
+    user.ws.send(JSON.stringify(message));
+  });
+}
 
-// Redirect to https
-app.get("*", (req, res, next) => {
-  if (req.headers["x-forwarded-proto"] !== "https" && env !== "development") {
-    return res.redirect(["https://", req.get("Host"), req.url].join(""));
-  }
-  next();
-});
+server.on("connection", (ws) => {
+  const userRef = { ws };
+  users.add(userRef);
+  ws.on("message", (message) => {
+    console.log(message);
+    try {
+      const data = JSON.parse(message);
+      if (typeof data.sender !== "string" || typeof data.body !== "string") {
+        console.error("Invalid message");
+        return;
+      }
 
-app.use(express.static(path.join(__dirname, "public")));
-app.use(express.static(path.join(__dirname, "node_modules")));
+      const messageToSend = {
+        sender: data.sender,
+        body: data.body,
+        sentAt: Date.now(),
+      };
 
-const server = https.createServer(credentials, app);
-server.listen(port, () => {
-  console.log(`listening on port ${port}`);
-});
-
-/**
- * Socket.io events
- */
-const io = socketIO(server);
-io.sockets.on("connection", function (socket) {
-  /**
-   * Log actions to the client
-   */
-  function log() {
-    const array = ["Server:"];
-    array.push.apply(array, arguments);
-    socket.emit("log", array);
-  }
-
-  /**
-   * Handle message from a client
-   * If toId is provided message will be sent ONLY to the client with that id
-   * If toId is NOT provided and room IS provided message will be broadcast to that room
-   * If NONE is provided message will be sent to all clients
-   */
-  socket.on("message", (message, toId = null, room = null) => {
-    console.log("Client " + socket.id + " said: ", message);
-
-    if (toId) {
-      console.log("From ", socket.id, " to ", toId, message.type);
-
-      io.to(toId).emit("message", message, socket.id);
-    } else if (room) {
-      console.log("From ", socket.id, " to room: ", room, message.type);
-
-      socket.broadcast.to(room).emit("message", message, socket.id);
-    } else {
-      console.log("From ", socket.id, " to everyone ", message.type);
-
-      socket.broadcast.emit("message", message, socket.id);
+      sendMessage(messageToSend);
+    } catch (e) {
+      console.error("Error parsing message", e);
     }
   });
-
-  let roomAdmin; // save admins socket id (will get overwritten if new room gets created)
-
-  /**
-   * When room gets created or someone joins it
-   */
-  socket.on("create or join", (room) => {
-    log("Create or Join room: " + room);
-
-    // Get number of clients in the room
-    const clientsInRoom = io.sockets.adapter.rooms.get(room);
-    let numClients = clientsInRoom ? clientsInRoom.size : 0;
-
-    if (numClients === 0) {
-      // Create room
-      socket.join(room);
-      roomAdmin = socket.id;
-      socket.emit("created", room, socket.id);
-    } else {
-      log("Client " + socket.id + " joined room " + room);
-
-      // Join room
-      io.sockets.in(room).emit("join", room); // Notify users in room
-      socket.join(room);
-      io.to(socket.id).emit("joined", room, socket.id); // Notify client that they joined a room
-      io.sockets.in(room).emit("ready", socket.id); // Room is ready for creating connections
-    }
-  });
-
-  /**
-   * Kick participant from a call
-   */
-  socket.on("kickout", (socketId, room) => {
-    if (socket.id === roomAdmin) {
-      socket.broadcast.emit("kickout", socketId);
-      io.sockets.sockets.get(socketId).leave(room);
-    } else {
-      console.log("not an admin");
-    }
-  });
-
-  // participant leaves room
-  socket.on("leave room", (room) => {
-    socket.leave(room);
-    socket.emit("left room", room);
-    socket.broadcast.to(room).emit("message", { type: "leave" }, socket.id);
-  });
-
-  /**
-   * When participant leaves notify other participants
-   */
-  socket.on("disconnecting", () => {
-    socket.rooms.forEach((room) => {
-      if (room === socket.id) return;
-      socket.broadcast.to(room).emit("message", { type: "leave" }, socket.id);
-    });
+  ws.on("close", (code, reason) => {
+    users.delete(userRef);
+    console.log(`Connection closed: ${code} ${reason}!`);
   });
 });
